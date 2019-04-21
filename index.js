b6c1fb2c1ba58bcfe0f0bb4183dd5fb14acfbc92
Roman Lubij <roman.lubij@gmail.com>
2019-04-21 17:36:42 +0200

    Somewhat working prototype

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const { Discord } = reqire(`discord.js`)
+const { Client } = require(`discord.js`)
+const conf = require(`./conf.json`)
+const bot = new Client({ disabledEvents: [`GUILD_SYNC`, `GUILD_UPDATE`, `CHANNEL_CREATE`, `CHANNEL_DELETE`, `CHANNEL_UPDATE`, `TYPING_START`, `RELATIONSHIP_ADD`, `RELATIONSHIP_REMOVE`] })
+
+bot.on(`ready`, () => {
+  console.log(`ready`)
+})
+
+bot.on(`message`, (msg, client) => {
+  const args = msg.content.slice(conf.prefix.length).split(' ')
+  const command = args.shift().toLowerCase()
+
+  msg.say = str => {
+    msg.channel.send(str)
+  }
+  if (!msg.content.startsWith(conf.prefix) || msg.author.bot) return
+
+  if (command === `count`) {
+    const count = { has: 0, nohas: 0 }
+    msg.guild.members.map(m => {
+      if (m.roles.some(r => r.name === args.join(` `))) {
+        count.has++
+      } else {
+        count.nohas++
+      }
+    })
+    msg.say(`${count.has} members have this role and ${count.nohas} don't have this role
+    So that means that approx. ${Math.round((100 * count.has) / msg.guild.memberCount)}% of members have it`)
+  }
+})
+
+bot.login(conf.token)

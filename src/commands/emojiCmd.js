04ce878480fdc4cf2fabd69ae660d737f628a43d
Roman Lubij <roman.lubij@gmail.com>
2019-10-22 20:05:31 +0200

    blep

---
+++ src/commands/emojiCmd.js
@@ -0,0 +1,34 @@
+const emojiCmd = {
+  invoke: `emoji`,
+  aliases: [],
+  desc: `Sends a custom emoji`,
+  help: `<emoji name>`,
+  type: `util`,
+  cmdFunc: (msg, args, client) => {
+    /* const emoji = client.emojis.map()
+    console.log(emoji)
+    return msg.channel.send('emoji') */
+
+    if (args.length === 1) {
+      const emoji = client.emojis.find(val => val.name === args[0])
+      const member = msg.guild.member(msg.author)
+
+      return msg.delete.then(msg.channel.send(`${!member.nickname ? msg.user.username : member.nickname}: ${emoji}`))
+    } else {
+      const send = (msg, str) => {
+        if (msg.guild.member(client.user).hasPermissions(`MANAGE_MESSAGES`)) {
+          return msg.channel.send(str).then(m => m.delete(1500)).then(msg.delete())
+        } else {
+          return msg.channel.send(str).then(m => m.delete(1500))
+        }
+      }
+      if (args.length > 1) {
+        return send(`Too many arguments!`)
+      } else {
+        return send(`Too few arguments!`)
+      }
+    }
+  }
+}
+
+exports.emojiCmd = emojiCmd
